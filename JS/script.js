554bf1dcf203dee7defe0ad7894a90691a3d89b0
miguelcarmona23 <miguelcarmona996@gmail.com>
2020-02-13 17:06:17 -0500

    Fixed function fixNav error

--- JS/script.js
+++ JS/script.js
@@ -2,7 +2,12 @@ const nav = document.querySelector('#main');
 const topOfNav = nav.offsetTop;
 
 function fixNav() {
-    if (window.scrollY >= topOfNav);
+    if (window.scrollY >= topOfNav) {
+        document.body.classList.add('fixed-nav');
+    } else {
+        document.body.classList.remove('fixed-nav');
+    }
 }
+
 
 window.addEventListener('scroll', fixNav);
