c45c1d12d2ef49745e2715803e3e528a0aec47b7
miguelcarmona23 <miguelcarmona996@gmail.com>
2020-02-13 17:03:23 -0500

    Added if statement to function fixNav

--- JS/script.js
+++ JS/script.js
@@ -2,7 +2,7 @@ const nav = document.querySelector('#main');
 const topOfNav = nav.offsetTop;
 
 function fixNav() {
-
+    if (window.scrollY >= topOfNav);
 }
 
 window.addEventListener('scroll', fixNav);
